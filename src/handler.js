c778874bac28694a0629c95df81aea6eca845919
Chetan Bhatt <chetanbhatt175@gmail.com>
2025-04-10 17:54:40 +0530

    latest changes

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,27 @@
+// 'use strict';
+
+// module.exports.generateRandonNumber = async (event) => {
+//   const randomNumber = parseint(Math.random()*100);
+//   console.log("The random generate number is :",randomNumber);
+//   return randomNumber;
+//   };
+
 'use strict';
 
-module.exports.generateRandonNumber = async (event) => {
-  const randomNumber = parseint(Math.random()*100);
-  console.log("The random generate number is :",randomNumber);
-  return randomNumber;
+const newrelic = require('newrelic-lambda-wrapper');
+
+const generate = async (event) => {
+  const randomNumber = parseInt(Math.random() * 100);
+  console.log("The randomly generated number is:", randomNumber);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Success",
+      number: randomNumber,
+    }),
   };
+};
+
+// Use newrelic-lambda-wrapper properly
+exports.generateRandonNumber = newrelic.wrap(generate);
